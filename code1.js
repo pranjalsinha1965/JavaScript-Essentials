0a2806feccc18b13852b428a4e1494aa126edae5
Pranjal Sinha <pranjalsinha1965@gmail.com>
2024-02-04 16:50:01 +0530

    todays codes, 4th February

--- code1.js
+++ code1.js
@@ -1,8 +1,11 @@
+//Note-1
+"use strict";
+//treat all JS code as newer version
+//through out the document we will be using camel casing 
 const user = {
     username: "hitesh",
     prices: 199
 }
-
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
@@ -12,13 +15,10 @@ handleObject({
     username: "sam",
     price: 399
 })
-
 const myNewArray = [200, 400, 100, 600]
-
 function returnSecondValue(getArray){
     return getArray[1]
 }
-
 // console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
 
@@ -31,56 +31,7 @@ if (true) {
     
 }
 
-
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-
-function one(){
-    const username = "hitesh"
-
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
-
-     two()
-
-}
-
-// one()
-
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
-}
-
-// console.log(username);
-
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
-
-
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
-
-
-
-// addTwo(5)
-// const addTwo = function(num){
-//     return num + 2
-// }
-
+//Note-2
 // number => 2 to power 53
 // bigint
 // string => ""
@@ -88,11 +39,9 @@ function addone(num){
 // null => standalone value
 // undefined => 
 // symbol => unique
-
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
-
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
@@ -101,18 +50,16 @@ let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
 
+//Note-3
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
 */
-
 // accountId = 2 // not allowed
-
 //  Primitive
-
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
+
 const scoreValue = 100.3
 
 const isLoggedIn = false
@@ -123,23 +70,122 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id === anotherId);
+console.log(typeof anotherId);
 
+//Note-3
+
+/* 
+var should not be used because of 
+block scope and functional scope problems 
+*/
+// we will be node.js as the javascript engine and not any browser 
+// always watch ecma script mdn for js 
+// undefined means the value has not been reassigned
+// And null is basically a representation of standalone values
 // const bigNumber = 3456543576654356754n
-
-
-
 // Reference (Non primitive)
-
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+//Datatype conversion
+//type-1: alphanumeric conversion
+const  score  = "33abc";
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+console.log(typeof score);
+console.log(typeof(score));
 
-console.log(typeof anotherId);
+const valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//type-2: bool`s conversion
+let isSignedIn = "Pranjal";
+let booleanIsSignedIn = Boolean(isSignedIn);
+console.log(booleanIsSignedIn);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//Data-Types
+//Primitive
+// 7 Types: String, Number, Boolean, null, undefined, symbol, bigint
+
+const numVar = 100;
+const scoreVal = 100.3;
+
+const isSignedOut = false;
+const tempOutside = null;
+
+const myId = Symbol('123');
+const bigNum = 3456789123990n;
+console.log(typeof bigNum);
+
+//Reference (Non Primitve)
+//Array, Objects, Functions
+
+const heroes= ["superman", "Spiderman", "blackpanther"];
+console.log(heroes);
+
+//Stack Primitive, Heap Non-Primitive
+//Datatype: Strings
+const myName = "Pranjal";
+const repoCount = 43;
+
+console.log(`Hello my name is $(myName) and my repo count is $(repoCount)`);
+
+const gameName = new String('hitesh-hc-com');
+
+//console.log(gameName[0])
+//console.log(gameName_.proto_);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+console.log(gameName(charAt(2)));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(newString);
+
+const newStringOne = "  Pranjal  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "WWW.google.com";
+console.log(url.replace('googl', 'mdn'));
+console.log(url.includes('pranjal'));
+console.log(gameName.split('-'));
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
